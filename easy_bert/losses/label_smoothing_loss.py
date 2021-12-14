66f78e99fd78f46b80bd2ccacb2c564110420798
waking95 <wang0624@foxmail.com>
2021-12-14 15:45:26 +0800

    label_smoothing_loss support ignore_index

--- easy_bert/losses/label_smoothing_loss.py
+++ easy_bert/losses/label_smoothing_loss.py
@@ -8,23 +8,28 @@ class LabelSmoothingCrossEntropy(nn.Module):
     def __init__(self, alpha=0.1, reduction='mean', ignore_index=-100):
         super(LabelSmoothingCrossEntropy, self).__init__()
         self.alpha = alpha
+
+        assert reduction in ('mean', 'sum')
         self.reduction = reduction  # 对batch维度的loss的处理策略，可以是mean或者sum
+
         self.ignore_index = ignore_index  # 忽略target中为ignore_index的位置
 
     def forward(self, logits, target):
         """计算标签平滑过的交叉熵loss，请参照公式"""
-        
+
         # 获取类别数K
         K = logits.size()[-1]
 
         # 对logits计算softmax得到概率，并取log
         log_preds = F.log_softmax(logits, dim=-1)
+        # 去掉ignore_index位置的结果
+        active_log_preds = log_preds[target != self.ignore_index]
 
         # 计算非target部分的loss
         if self.reduction == 'sum':
-            no_target_loss = -log_preds.sum()  # 同时沿着batch和label维度求和
+            no_target_loss = -active_log_preds.sum()  # 同时沿着batch和label维度求和
         else:
-            no_target_loss = -log_preds.sum(dim=-1)  # 沿着label维度求和
+            no_target_loss = -active_log_preds.sum(dim=-1)  # 沿着label维度求和
             if self.reduction == 'mean':
                 no_target_loss = no_target_loss.mean()  # 沿着batch维度平均
         no_target_loss = self.alpha / K * no_target_loss  # 乘上非target部分的yi
--- tests/test_label_smoothing_ce.py
+++ tests/test_label_smoothing_ce.py
@@ -6,8 +6,25 @@ from easy_bert.losses.label_smoothing_loss import LabelSmoothingCrossEntropy
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test(self):
+        print('test~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         lsce = LabelSmoothingCrossEntropy()
+        logits = torch.randn(4, 2)  # (batch_size=4, label_size=2)
+        target = torch.tensor([0, 1, 1, 0])
+        loss = lsce(logits, target)
+        print(loss)
+
+    def test_ignore_index(self):
+        print('test_ignore_index~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        lsce = LabelSmoothingCrossEntropy(ignore_index=-1)
+        logits = torch.randn(6, 2)  # (seq_len=4, label_size=2)
+        target = torch.tensor([-1, 0, 1, 1, 0, -1])  # 序列标注一般首尾，即[CLS][SEP]部分用-1填充，计算loss时忽略它们
+        loss = lsce(logits, target)
+        print(loss)
+
+    def test_reduction(self):
+        print('test_reduction~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        lsce = LabelSmoothingCrossEntropy(reduction='sum')
         logits = torch.randn(4, 2)  # (batch_size=4, label_size=2)
         target = torch.tensor([0, 1, 1, 0])
         loss = lsce(logits, target)
