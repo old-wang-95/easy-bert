71ec184fc64c3907d3522a54c197a97f55f638da
waking95 <wang0624@foxmail.com>
2021-12-20 11:52:48 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easy-bert',
-    version='0.2.0',
+    version='0.3.0',
     author='waking95',
     author_email="wang0624@foxmail.com",
     license='MIT',
